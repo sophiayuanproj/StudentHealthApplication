3cef4e37f12fb5feacbc234bb649c36e4e2debb0
Paul Zhou <crowbargel@gmail.com>
2022-09-30 01:28:35 -0700

    Update sleep model

--- sleep/src/main/java/com/example/sleep/model/SleepModel.java
+++ sleep/src/main/java/com/example/sleep/model/SleepModel.java
@@ -1,6 +1,10 @@
 package com.example.sleep.model;
 
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.Duration;
